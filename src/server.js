d6ccef2d95586ad6099cb9d6e3e93eea0785eab1
totoruo9 <55140745+totoruo9@users.noreply.github.com>
2021-07-14 09:28:11 +0900

    [install morgan] morgan을 설치하여 middleware 사용

--- src/server.js
+++ src/server.js
@@ -1,36 +1,21 @@
 import express from "express";
+import morgan from "morgan";
 
 const PORT = 4000;
 
 const app = express();
-
-const logger = (req, res, next) => {
-    console.log(`${req.method} ${req.url}`);
-    next();
-}
+const logger = morgan("dev");
 
 const handleHome = (req, res) => {
     return res.send("<h1>hello node</h1>");
 };
-const privateMiddlewaare = (req, res, next) => {
-    const url = req.url;
-    if(url === "/protected"){
-        return res.send("<h1>Not Allowed</h1>");
-    }
-    next();
-}
 const handleLogin = (req, res) => {
     return res.send({message: "Login here"});
 }
-const handleProtected = (req, res) => {
-    return res.send("Wellcome");
-}
+
 app.use(logger);
-app.use(privateMiddlewaare);
 app.get("/", handleHome);
 app.get("/login", handleLogin);
-app.get("/protected", handleProtected);
-
 
 const handleListening = () => console.log(`Server listenting on port http://localhost:${PORT}`);
 
