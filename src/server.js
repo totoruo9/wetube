1c1621f24c0a19e3eb4a4784ec91233758c93340
totoruo9 <55140745+totoruo9@users.noreply.github.com>
2021-07-14 10:38:05 +0900

    [router] firet time to use router

--- src/server.js
+++ src/server.js
@@ -5,17 +5,26 @@ const PORT = 4000;
 
 const app = express();
 const logger = morgan("dev");
-
-const handleHome = (req, res) => {
-    return res.send("<h1>hello node</h1>");
-};
-const handleLogin = (req, res) => {
-    return res.send({message: "Login here"});
-}
-
 app.use(logger);
-app.get("/", handleHome);
-app.get("/login", handleLogin);
+
+const globalRouter = express.Router();
+const handleHome = (req, res) => res.send("Home");
+globalRouter.get("/", handleHome);
+
+const userRouter = express.Router();
+const handleEditUser = (req, res) => res.send("Edit User");
+userRouter.get("/edit", handleEditUser);
+
+const videoRouter = express.Router();
+const handleWatchVideo = (req, res) => res.send("Watch Video");
+videoRouter.get("/watch", handleWatchVideo);
+
+
+app.use("/", globalRouter);
+app.use("/videos", videoRouter);
+app.use("/users", userRouter);
+
+
 
 const handleListening = () => console.log(`Server listenting on port http://localhost:${PORT}`);
 
