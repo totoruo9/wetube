3658b5ebc3c071e5688d3f4c124602824d4a6f5f
Jin Hyuk <wlsgurrla10@naver.com>
2021-07-17 13:15:19 +0900

    [mongodb edit video] video의 정보를 업데이트 해준다 현재 적용한 방법은 하나하나 찾아서 업데이트 해주어야 하기에 좋은 방식이 아니다. 이후 커밋 될 코드를 참고하자

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -12,15 +12,30 @@ export const home = async (req, res) => {
 export const watch = async(req, res) => {
     const {id} = req.params;
     const video = await Video.findById(id);
+    if(!video){
+        return res.render("404", {pageTitle: "Video not found"});    
+    }
     return res.render("watch", { pageTitle: `[Watching] ${video.title}`, video });
 }
-export const getEdit = (req, res) => {
+export const getEdit = async(req, res) => {
     const {id} = req.params;
-    return res.render("edit", {pageTitle: `Editing` });
+    const video = await Video.findById(id);
+    if(!video){
+        return res.render("404", {pageTitle: "Video not found"})
+    }
+    return res.render("edit", {pageTitle: `[Edit] ${video.title}`, video });
 };
-export const postEdit = (req, res) => {
+export const postEdit = async(req, res) => {
     const {id} = req.params;
-    const {title} = req.body;
+    const {title, description, hashtags} = req.body;
+    const video = await Video.findById(id);
+    if(!video){
+        return res.redirect("404", {pageTitle: `Video not found`});
+    }
+    video.title = title;
+    video.description = description;
+    video.hashtags = hashtags.split(",").map(word => word.startsWith('#') ? word : `#${word}`);
+    await video.save();
     return res.redirect(`/videos/${id}`);
 };
 
@@ -34,7 +49,7 @@ export const postUpload = async (req, res) => {
         await Video.create ({
             title,
             description,
-            hashtags: hashtags.split(",").map(word => `#${word}`),
+            hashtags: hashtags.split(",").map(word => word.startsWith('#') ? word : `#${word}`),
         });
         return res.redirect('/');
     }catch(error){
