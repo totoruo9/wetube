c399931be75809c2559923d6f98365653ae827ee
Jin Hyuk <wlsgurrla10@naver.com>
2021-07-18 11:33:32 +0900

    [User(create models, views, controllers) password hash(bcrypt)] 사용자 계정을 만들고 패스워드를 bcrypt를 이용해 hash시켜 db에 저장시켜준다.

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,4 +1,13 @@
-export const join = (req, res) => res.send("Join");
+import User from "../models/User"
+
+export const getJoin = (req, res) => {
+    res.render("join", {pageTitle: "Join"})
+};
+export const postJoin = async(req, res) => {
+    const {name, username, email, password, location} = req.body;
+    await User.create({name, username, email, password, location});
+    return res.redirect("/login");
+};
 export const edit = (req, res) => res.send("edit");
 export const remove = (req, res) => res.send("remove");
 export const login = (req, res) => res.send("Log In")
