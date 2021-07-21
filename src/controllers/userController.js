568cf5d8613a62d4c73361b240a9a9f9fe0aa658
totoruo9 <55140745+totoruo9@users.noreply.github.com>
2021-07-21 16:39:51 +0900

    [github login] db에 github의 이메일 정보와 일치하는 사용자가 있는지 확인하고 일치하는 사용자가 있으면 바로 로그인 할 수 있게 해주고, 없다면 빈 패스워드 값을 가진 계정으로 생성하여 로그인 시켜준다.

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -101,13 +101,31 @@ export const finishGithubLogin = async(req, res) => {
                 }
             })
         ).json();
-        const email = emailData.find(
+        const emailObj = emailData.find(
             (email) => email.primary === true && email.verified === true
         );
-        if(!email){
+        if(!emailObj){
             return res.redirect("/login");
         }
-        console.log(email);
+        const existingUser = await User.findOne({email: emailObj.email});
+
+        if(existingUser){
+            req.session.loginState = true;
+            req.session.user = existingUser;
+            return res.redirect("/");
+        }else {
+            const user = await User.create({
+                name: userData.name || userData.login,
+                socialOnly: true,
+                username: userData.login,
+                email: emailObj.email,
+                password:"",
+                location: userData.location,
+            });
+            req.session.loginState = true;
+            req.session.user = user;
+            return res.redirect("/");
+        }
     }else{
         return res.redirect("/login");
     }
