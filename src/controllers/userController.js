4364a8b0c73df008f142612d197b8f917f44c786
totoruo9 <55140745+totoruo9@users.noreply.github.com>
2021-07-27 16:01:22 +0900

    [video owner] 유저 프로필에 들어가면 해당 유저가 올린 비디오를 보여준다. (id 값을 이용해 찾는다.)

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,6 +1,7 @@
 import User from "../models/User"
 import bcrypt from "bcrypt";
 import fetch from "node-fetch";
+import Video from "../models/Video";
 
 export const getJoin = (req, res) => {
     res.render("users/join", {pageTitle: "Join"})
@@ -205,5 +206,7 @@ export const see = async(req, res) => {
     if(!user){
         return res.status(404).render("404", {pageTitle: "User not found!"});
     }
-    return res.render("users/profile", {pageTitle: `${user.name} Profile`, user});
+    const videos = await Video.find({owner: id});
+    console.log(videos);
+    return res.render("users/profile", {pageTitle: `${user.name} Profile`, user, videos});
 }
--- src/middleware.js
+++ src/middleware.js
@@ -4,7 +4,6 @@ export const localsMiddleware = (req, res, next) => {
     res.locals.loginState = Boolean(req.session.loginState);
     res.locals.siteName = "Wetube";
     res.locals.user = req.session.user || {};
-    console.log(res.locals.user)
     next();
 }
 
