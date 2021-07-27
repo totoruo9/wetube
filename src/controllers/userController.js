4edef4d5eceda55b89921fa286f11e335774c11e
totoruo9 <55140745+totoruo9@users.noreply.github.com>
2021-07-27 13:15:46 +0900

    [Upload Video / See User Profile] multer을 이용해 비디오 업로드 기능 구현, 아바타와 비디오 업로드의 미들웨어를 구분하여 생성, 아파타의 파일크기를 3mb, 비디오의 업로드 크기를 10mb로 제한
    유저 정보를 볼 수 있는 페이지 생성

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -199,4 +199,11 @@ export const postChangePassword = (req, res) => {
 }
 
 
-export const see = (req, res) => res.send("See");
+export const see = async(req, res) => {
+    const {id} = req.params;
+    const user = await User.findById(id);
+    if(!user){
+        return res.status(404).render("404", {pageTitle: "User not found!"});
+    }
+    return res.render("users/profile", {pageTitle: `${user.name} Profile`, user});
+}
--- src/routers/userRouter.js
+++ src/routers/userRouter.js
@@ -1,11 +1,11 @@
 import express from "express";
 import { getEdit, postEdit, see, logout, startGithublogin, finishGithubLogin, getChangePassword, postChangePassword } from "../controllers/userController";
-import { protectorMiddleware, publicOnlyMiddleware, uploadFiles } from "../middleware";
+import { avatarUpload, protectorMiddleware, publicOnlyMiddleware } from "../middleware";
 
 const userRouter = express.Router();
 
 userRouter.get("/logout", protectorMiddleware, logout);
-userRouter.route("/edit").all(protectorMiddleware).get(getEdit).post(uploadFiles.single("avatar"), postEdit);
+userRouter.route("/edit").all(protectorMiddleware).get(getEdit).post(avatarUpload.single("avatar"), postEdit);
 userRouter.route("/change-password").all(protectorMiddleware).get(getChangePassword).post(postChangePassword);
 userRouter.get("/github/start", publicOnlyMiddleware, startGithublogin);
 userRouter.get("/github/finish", publicOnlyMiddleware, finishGithubLogin);
