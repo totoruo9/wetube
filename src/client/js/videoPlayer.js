91ebe5d1d61fe17d18a5e4bea7fdc731abe295c8
totoruo9 <55140745+totoruo9@users.noreply.github.com>
2021-08-02 12:44:23 +0900

    [ handle video player ] 비디오 버튼 생성 및 시작과 정지 버튼 구현

--- src/client/js/videoPlayer.js
+++ src/client/js/videoPlayer.js
@@ -1,1 +1,30 @@
-console.log("Video Player")
+const video = document.querySelector("video");
+const playBtn = document.querySelector("#play");
+const muteBtn = document.querySelector("#mute");
+const time = document.querySelector("#time");
+const volume = document.querySelector("#volume");
+
+const handlePlayClick = (e) => {
+    if(video.paused){
+        video.play();
+    }else{
+        video.pause();
+    }
+}
+
+const handlePause = (e) => {
+    playBtn.innerText = "Play";
+}
+
+const handlePlay = (e) => {
+    playBtn.innerText = "Pause";
+}
+
+const handleMute = (e) => {
+
+}
+
+playBtn.addEventListener("click", handlePlayClick);
+muteBtn.addEventListener("click", handleMute);
+video.addEventListener("pause", handlePause);
+video.addEventListener("play", handlePlay);
