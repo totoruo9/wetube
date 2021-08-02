2f1ea5c5f9d7f235bb37736c07df7edb95a60652
totoruo9 <55140745+totoruo9@users.noreply.github.com>
2021-08-02 14:26:13 +0900

    [ video player mouse stop ] mousemove 내부에 setTimeout을 이용해 class를 제거하는 코드를 계속해서 실행하고 clearTimeout을 통해 3초동안 움직이지 않으면 setTimeout이 실행되도록 설정해서 마우스가 멈추면 controls가 보이지 않도록 만들어 주었다.(실제로 보이지 않게 구현하는 것은 다음 커밋에서 작업 예정)

--- src/client/js/videoPlayer.js
+++ src/client/js/videoPlayer.js
@@ -10,6 +10,7 @@ const videoContainer = document.querySelector("#videoContainer");
 const videoControls = document.querySelector("#videoControls");
 
 let controlsTimeout = null;
+let controlsMovementTimeout = null;
 let volumeValue = .5;
 video.volume = .5;
 
@@ -68,21 +69,26 @@ const handleFullScreen = () => {
     }else{
         videoContainer.requestFullscreen();
         fullScreen.innerText = "Exit Full Screen";
-    }
+    };
 };
+
+const hideControls = () => videoControls.classList.remove("showing");
 
 const handleMouseMove = () => {
     if(controlsTimeout){
         clearTimeout(controlsTimeout);
         controlsTimeout = null;
-    }
+    };
+    if(controlsMovementTimeout){
+        clearTimeout(controlsMovementTimeout);
+        controlsMovementTimeout = null;
+    };
     videoControls.classList.add("showing");
+    controlsMovementTimeout = setTimeout(hideControls, 3000);
 };
 
 const handleMouseLeave = () => {
-    controlsTimeout = setTimeout(()=>{
-        videoControls.classList.remove("showing");
-    }, 3000);
+    controlsTimeout = setTimeout(hideControls, 3000);
 }
 
 playBtn.addEventListener("click", handlePlayClick);
