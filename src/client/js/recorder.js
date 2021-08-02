f1dc83ff4b7565fced2af10bb81dc03a1e2d4159
Jin Hyuk <wlsgurrla10@naver.com>
2021-08-02 21:48:30 +0900

    [Recorde video] 1. 녹화되고 있는 화면을 브라우저에 노출시킨다. 2. 화면 녹화를 시작하고 10초 뒤 종료되도록 설정한다.

--- src/client/js/recorder.js
+++ src/client/js/recorder.js
@@ -1,8 +1,42 @@
 const startBtn = document.querySelector("#startBtn");
+const video = document.querySelector("#preview");
 
-const handleStart = async() => {
-    const stream = await navigator.mediaDevices.getUserMedia({audio:true, video:true});
-    console.log(stream);
-}
+let stream;
+
+const handleStop = () => {
+    startBtn.innerText = "Play Recording";
+    startBtn.removeEventListener("click", handleStop);
+    startBtn.addEventListener("click", handleStart);
+};
+
+const handleStart = () => {
+    startBtn.innerText = "Stop Recording"
+    startBtn.removeEventListener("click", handleStart);
+    startBtn.addEventListener("click", handleStop);
+    const recorder = new MediaRecorder(stream);
+    
+    recorder.ondataavailable = (e) => {
+        console.log("Recorder Done");
+        console.log(e);
+        console.log(e.data);
+    }
+    console.log(recorder);
+    recorder.start();
+    console.log(recorder);
+    setTimeout(()=>{
+        recorder.stop();
+    }, 10000)
+};
+
+const init = async() => {
+    stream = await navigator.mediaDevices.getUserMedia({
+        audio:false,
+        video:true
+    });
+    video.srcObject = stream;
+    video.play();
+};
+
+init();
 
 startBtn.addEventListener("click", handleStart);
