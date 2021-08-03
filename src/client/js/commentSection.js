e2c169bfdba01a04288b28eb80e677e78a572cb9
Jin Hyuk <wlsgurrla10@naver.com>
2021-08-04 00:21:59 +0900

    [ Comment ] Real Time Comment js로 댓글을 달면 html요소를 생성해 가상의 댓글을 만들어 노출시켜 실시간 댓글창 처럼 구현했다

--- src/client/js/commentSection.js
+++ src/client/js/commentSection.js
@@ -1,22 +1,43 @@
 const videoContainer = document.getElementById("videoContainer");
 const form = document.getElementById("commentForm");
 
+const addComment = (text) => {
+    const videoComments = document.querySelector(".video__comments ul");
+
+    const newComment = document.createElement("li");
+    newComment.className = "video__comment";
+
+    const icon = document.createElement("i");
+    icon.className = "fas fa-comment";
+
+    const span = document.createElement("span");
+    span.innerText = `  ${text}`;
+    
+    newComment.appendChild(icon);
+    newComment.appendChild(span);
+    videoComments.prepend(newComment);
+}
+
 const handleSubmit = async (event) => {
     event.preventDefault();
     const textarea = form.querySelector("textarea");
     const text = textarea.value;
     const videoId = videoContainer.dataset.id;
-    if(text !== ""){
-        await fetch(`/api/videos/${videoId}/comment`, {
-            method: "POST", 
-            headers: {
-                "Content-Type": "application/json",
-            },
-            body: JSON.stringify({text}),
-        });
+    if(text === ""){
+        return;
     }
+
+    const {status} = await fetch(`/api/videos/${videoId}/comment`, {
+        method: "POST", 
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify({text}),
+    });
+    if(status === 201){
+        addComment(text);
+    };
     textarea.value = "";
-    window.location.reload();
 };
 
 if(form){
