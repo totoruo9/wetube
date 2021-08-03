b551bb2c06b38d80bbe975b576493b549a62fc54
Jin Hyuk <wlsgurrla10@naver.com>
2021-08-03 22:38:24 +0900

    [ Comment ] fontend에서 fetch를 통해서 보낸 json파일을 express가 읽을 수 있도록 설정해 주었다

--- src/client/js/commentSection.js
+++ src/client/js/commentSection.js
@@ -6,12 +6,15 @@ const handleSubmit = (event) => {
     const textarea = form.querySelector("textarea");
     const text = textarea.value;
     const videoId = videoContainer.dataset.id;
-    fetch(`/api/videos/${videoId}/comment`, {
-        method: "POST", 
-        body: {
-            text,
-        }
-    });
+    if(text === ""){
+        fetch(`/api/videos/${videoId}/comment`, {
+            method: "POST", 
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify({text}),
+        });
+    };
 };
 
 if(form){
