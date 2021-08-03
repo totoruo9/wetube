5339170cc4278a11f4dacdc10286958045fb8967
Jin Hyuk <wlsgurrla10@naver.com>
2021-08-04 00:46:42 +0900

    [ Comment ] fake comment에서 댓글 삭제 기능을 구현하기 위해서는 id값을 backend에서 가져와서 부여해 주어야 하는데, 이러한 과정을 진행하기 위한 코드들이다. sendStatus -> status.json()으로 변경하여 프론트에서 데이터를 사용할 수 있게 해주고 fetch를 통해 백앤드가 보낸 데이터를 확인하고 body 내부의 내용을 추출하기위해 json 메소드를 사용한다.

--- src/client/js/commentSection.js
+++ src/client/js/commentSection.js
@@ -1,20 +1,27 @@
 const videoContainer = document.getElementById("videoContainer");
 const form = document.getElementById("commentForm");
 
-const addComment = (text) => {
+let json;
+
+const addComment = (text, id) => {
     const videoComments = document.querySelector(".video__comments ul");
 
     const newComment = document.createElement("li");
+    newComment.dataset.id = id;
     newComment.className = "video__comment";
 
     const icon = document.createElement("i");
     icon.className = "fas fa-comment";
 
     const span = document.createElement("span");
-    span.innerText = `  ${text}`;
+    span.innerText = ` ${text}`;
+
+    const span2 = document.createElement("span");
+    span2.innerText = "❌"
     
     newComment.appendChild(icon);
     newComment.appendChild(span);
+    newComment.appendChild(span2);
     videoComments.prepend(newComment);
 }
 
@@ -27,17 +34,19 @@ const handleSubmit = async (event) => {
         return;
     }
 
-    const {status} = await fetch(`/api/videos/${videoId}/comment`, {
+    const response = await fetch(`/api/videos/${videoId}/comment`, {
         method: "POST", 
         headers: {
             "Content-Type": "application/json",
         },
         body: JSON.stringify({text}),
     });
-    if(status === 201){
-        addComment(text);
+    
+    if(response.status === 201){
+        textarea.value = "";
+        const {newCommentId} = await response.json();
+        addComment(text, newCommentId);
     };
-    textarea.value = "";
 };
 
 if(form){
