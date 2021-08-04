335189cb73273542ad22626c78db5996af50f47c
Jin Hyuk <wlsgurrla10@naver.com>
2021-08-05 00:23:08 +0900

    [ Build fronend ] webpack의 설정을 develpoment -> production으로 변경 및 watch를 제거하여 build 해주어야한다.

--- webpack.config.js
+++ webpack.config.js
@@ -13,8 +13,6 @@ module.exports = {
         recorder: BASE_JS+"recorder.js",
         commentSection: BASE_JS+"commentSection.js",
     },
-    mode: "development",
-    watch: true,
     output: {
         filename: "js/[name].js",
         path: path.resolve(__dirname, "assets"),
